0194dcf5e958cd4b52afa37d19058c9aa40ce4b3
ahmadov-mahammad <dev.ahmadov.mahammad@gmail.com>
2026-02-23 16:30:40 +0400

    initial commit message

---
+++ LanLinker.Desktop/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace LanLinker.Desktop.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ LanLinker.Desktop/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace LanLinker.Desktop.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
